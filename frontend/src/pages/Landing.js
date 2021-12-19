7259a3a5826e56faff177c47e309523f654009f9
marcelkolarcik <marcelkolarcik@gmail.com>
2021-12-19 10:54:21 +0000

    fixed on the air movies and series not rendering

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -6,6 +6,7 @@ import HeroTitle from "../components/HeroTitles/LandingTitle";
 export default function Landing(props) {
     const movies = props.movies
     const groupedMovies = props.groupedMovies
+    console.log(groupedMovies)
 
 
     return (
@@ -17,13 +18,13 @@ export default function Landing(props) {
 
                 <MovieScroller id={'top_rated'} title={'Top rated'} link={'/search/movie/top-rated'} movies={groupedMovies['mt']}/>
 
-                <MovieScroller id={'on_the_air'} title={'On the air'} link={'/search/movie/on-the-air'} movies={movies}/>
+                <MovieScroller id={'on_the_air'} title={'On the air'} link={'/search/movie/on-the-air'} movies={groupedMovies['ma']}/>
 
                  <MovieScroller id={'tv_popular'} title={'TV Popular'} link={'/search/series/popular'} movies={groupedMovies['sp']}/>
 
                 <MovieScroller id={'tv_top_rated'} title={'TV Top rated'} link={'/search/series/top-rated'} movies={groupedMovies['st']}/>
 
-                <MovieScroller id={'tv_on_the_air'} title={'TV On the air'} link={'/search/series/on-the-air'} movies={movies}/>
+                <MovieScroller id={'tv_on_the_air'} title={'TV On the air'} link={'/search/series/on-the-air'} movies={groupedMovies['sa']}/>
 
             </div>
         </>);
